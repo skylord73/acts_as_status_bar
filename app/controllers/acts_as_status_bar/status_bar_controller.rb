b1b8d28cb11980dc510cb3c0a0e38abfb4bf26b8
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2013-02-07 16:27:47 +0100

    a

--- app/controllers/acts_as_status_bar/status_bar_controller.rb
+++ app/controllers/acts_as_status_bar/status_bar_controller.rb
@@ -3,7 +3,7 @@ module ActsAsStatusBar
     def edit
       @status_bar = ActsAsStatusBar::StatusBar.find(params[:id])
       respond_to do |format|
-        format.html{render :inline => @status_bar.inspect}
+        format.html
         format.xml {render :inline => @status_bar.to_status_bar}
       end
     end
