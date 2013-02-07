b61f8eb81be2c7646e140b058324b36b49f81e44
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2013-02-07 18:53:38 +0100

    a

--- app/controllers/acts_as_status_bar/status_bar_controller.rb
+++ app/controllers/acts_as_status_bar/status_bar_controller.rb
@@ -1,7 +1,8 @@
 module ActsAsStatusBar
   class StatusBarController < ::ApplicationController
-    @status_bar = ActsAsStatusBar::StatusBar.find(session,params[:id])
+
     def edit
+      @status_bar = ActsAsStatusBar::StatusBar.find(session,params[:id])
       respond_to do |format|
         format.html
         format.xml {render :inline => ActsAsStatusBar::StatusBar.to_xml(@status_bar)}
