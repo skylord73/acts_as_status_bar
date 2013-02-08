b3e77965f747b7c78edd2915a56dc6401443b3e6
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2013-02-08 09:42:29 +0100

    a

--- app/controllers/acts_as_status_bar/status_bar_controller.rb
+++ app/controllers/acts_as_status_bar/status_bar_controller.rb
@@ -3,6 +3,9 @@ module ActsAsStatusBar
 
     def edit
       @status_bar = ActsAsStatusBar::StatusBar.new(params[:id])
+      mylog("edit: status_bar:#{@status_bar.inspect}")
+      mylog("edit1: ids:#{@status_bar.ids.inspect}")
+      mylog("edit3: max:#{@status_bar.max.inspect}")
       respond_to do |format|
         format.html
         format.xml {render :inline => @status_bar.to_xml}
