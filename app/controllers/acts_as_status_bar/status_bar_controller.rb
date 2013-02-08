ca7d012681649b4ea798ee861cc787fc30341ae1
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2013-02-08 15:58:11 +0100

    a

--- app/controllers/acts_as_status_bar/status_bar_controller.rb
+++ app/controllers/acts_as_status_bar/status_bar_controller.rb
@@ -4,11 +4,8 @@ module ActsAsStatusBar
       @status_bars=ActsAsStatusBar::StatusBar.all
     end
     
-    def edit
+    def show
       @status_bar = ActsAsStatusBar::StatusBar.new(params[:id].to_i)
-      mylog("edit: status_bar:#{@status_bar.inspect}")
-      mylog("edit1: ids:#{@status_bar.ids.inspect}")
-      mylog("edit3: max:#{@status_bar.max.inspect}")
       respond_to do |format|
         format.html
         format.xml {render :inline => @status_bar.to_xml}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
    namespace :acts_as_status_bar do
-     resources :status_bar, :only =>[:index, :edit, :destroy] do
+     resources :status_bar, :only =>[:index, :show, :destroy] do
  	     delete 'destroy_all', :on => :collection
  	   end
   end
