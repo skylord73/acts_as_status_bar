f914912075709fb795767f48dee3ea60c39eb904
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2013-02-10 20:10:31 +0100

    a

--- app/helpers/acts_as_status_bar_helper.rb
+++ app/helpers/acts_as_status_bar_helper.rb
@@ -17,8 +17,8 @@ module ActsAsStatusBarHelper
     frequency = status_bar.frequency
     stylesheet_link_tag('acts_as_status_bar')+
     content_tag(:div, :id => "acts-as-status-bar-container", :align => 'center') do
-      content_tag(:p, '', :id => 'acts-as-status-bar-message') +
-      content_tag(:p, '', :id => 'acts-as-status-bar-value') +
+      content_tag(:p, '...', :id => 'acts-as-status-bar-message') +
+      content_tag(:p, '...', :id => 'acts-as-status-bar-value') +
       content_tag(:div,
         content_tag(:div,'', :id => "acts-as-status-bar"),
         :id => "acts-as-status-bar-progress-bar", :align => 'left'
