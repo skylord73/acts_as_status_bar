1cbd4b5b3811f4428cbc26fb9d550fc59a739d98
dennis.varotto <dennis.varotto@nest2.com>
2013-02-14 14:15:20 +0100

    d

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
    namespace :acts_as_status_bar do
      resources :status_bar, :only =>[:index, :show, :destroy] do
  	     delete 'destroy_all', :on => :collection
-       #
-       delete 'destroy', :on => :member
  	   end
   end
 end
