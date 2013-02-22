b61479a7345e0b3bcb1dd92acab15a760d225be1
dennis.varotto <dennis.varotto@nest2.com>
2013-02-22 14:14:31 +0100

    d

--- spec/acts_as_status_bar_spec.rb
+++ spec/acts_as_status_bar_spec.rb
@@ -18,7 +18,7 @@ class MyClass < ActiveRecord::Base
   def initialize(*args)
     @options = args.extract_options!
     @status_bar_id = @options.delete[:id] if @options[:id]
-    status_bar_init(@status_bar_id)
+    status_bar_init(@status_bar_id) { }
     # super
   end
   
