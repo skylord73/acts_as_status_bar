4dcb289df3c1b2b875e300324de7607643290bdf
dennis.varotto <dennis.varotto@nest2.com>
2013-02-22 12:57:35 +0100

    d

--- spec/acts_as_status_bar_spec.rb
+++ spec/acts_as_status_bar_spec.rb
@@ -12,9 +12,9 @@ class MyClass < ActiveRecord::Base
   
   MAX = 100
   
-  def initialize(*options) 
+  def initialize
     status_bar_init(self)
-    # super
+    super
   end
   
   def clear_bar
